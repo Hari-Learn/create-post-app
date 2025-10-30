38307091297f8b2dcc3b6ae86c2304faf786ac03
Hari-repo <hariharanuk810@gmail.com>
2025-10-30 21:04:28 +0530

    Post Item for single post initial created

---
+++ src/comp/PostItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostItem = () => {
+    return(
+        <div>
+            <h2>have to do this post item for indidual items</h2>
+        </div>
+    )
+}
+
+export default PostItem;
